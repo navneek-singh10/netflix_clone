7c56e19ec5ed58abb353ca28143c1a02fbf22ebe
navneek-singh10 <navdoad01@gmail.com>
2022-07-16 19:52:38 +0530

    edited

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import {useAuth0} from '@auth0/auth0-react';
 
 function App() {
   const {user, isAuthenticated, isLoading} = useAuth0() ;
-
   return (
     <BrowserRouter>
       <div className="app">
